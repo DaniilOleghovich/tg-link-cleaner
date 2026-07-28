751e12afd8e84a439ce47a4b8536f0986a4fd039
Daniil <dendilord37@gmail.com>
2026-07-28 14:24:45 +0300

    Removed ability to add user to whitelist. Only admins or creator can send links in chat

--- src/handlers/commands.js
+++ src/handlers/commands.js
@@ -1,23 +1,16 @@
-import { getChatSettings, setFilterEnabled, ensureChat } from '../db/repositories/chats.js';
-import { addWhitelistDomain, removeWhitelistDomain, listWhitelistDomains } from '../db/repositories/whitelist.js';
-import { getDomain } from '../utils/link-detector.js';
-import {
-    addWhitelistUser,
-    removeWhitelistUser,
-    listWhitelistUsers,
-} from '../db/repositories/whitelist.js';
+import {ensureChat, getChatSettings, setFilterEnabled} from '../db/repositories/chats.js';
+import {addWhitelistDomain, listWhitelistDomains, removeWhitelistDomain} from '../db/repositories/whitelist.js';
+import {getDomain} from '../utils/link-detector.js';
 
 export async function cmdSettings(ctx) {
     await ensureChat(ctx.chat.id);
     const settings = await getChatSettings(ctx.chat.id);
     const domains = await listWhitelistDomains(ctx.chat.id);
-    const userIds = await listWhitelistUsers(ctx.chat.id);
 
     await ctx.reply(
         `Настройки чата:\n +
-    Фильтр ссылок: ${settings.filter_enabled ? 'включен' : 'выключен'}\n +
-    Разрешённые домены: ${domains.length ? domains.join(', ') : 'нет'}\n +
-    Разрешённые пользователи (ID): ${userIds.length ? userIds.join(', ') : 'нет'}`
+              Фильтр ссылок: ${settings.filter_enabled ? 'включен' : 'выключен'}\n +
+              Разрешённые домены: ${domains.length ? domains.join(', ') : 'нет'}`
 );
 }
 
@@ -49,25 +42,4 @@ export async function cmdWhitelistRemove(ctx) {
 
     await removeWhitelistDomain(ctx.chat.id, domain);
     await ctx.reply(`Домен ${domain} удалён из белого списка.`);
-}
-
-export async function cmdWhitelistUserAdd(ctx) {
-    const target = ctx.message.reply_to_message?.from;
-    if (!target) {
-        return ctx.reply('Ответьте этой командой на сообщение пользователя, которого хотите добавить в белый список.');
-    }
-
-    await ensureChat(ctx.chat.id);
-    await addWhitelistUser(ctx.chat.id, target.id);
-    await ctx.reply(`Пользователь ${target.first_name} добавлен в белый список.`);
-}
-
-export async function cmdWhitelistUserRemove(ctx) {
-    const target = ctx.message.reply_to_message?.from;
-    if (!target) {
-        return ctx.reply('Ответьте этой командой на сообщение пользователя, которого хотите убрать из белого списка.');
-    }
-
-    await removeWhitelistUser(ctx.chat.id, target.id);
-    await ctx.reply(`Пользователь ${target.first_name} удалён из белого списка.`);
 }
